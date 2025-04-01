df16c18dde37487d044cad492968ff5076a6cb6a
NikolayMMM <6atape9i1@gmail.com>
2025-04-01 12:34:53 +0300

    Добавил промоты(рекламные акции)

---
+++ app/repository/promotional_repository.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import Session
+from app.models.promotional_models import Promotional
+from typing import Optional
+from datetime import date
+
+class PromotionalRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def create_promotional(self, promotion_name: str, discount_type: str, discount_value: float, start_date: date, end_date: date, applicable_product_ids: str) -> Promotional:
+        promotional = Promotional(
+            promotion_name=promotion_name,
+            discount_type=discount_type,
+            discount_value=discount_value,
+            start_date=start_date,
+            end_date=end_date,
+            applicable_product_ids=applicable_product_ids
+        )
+
+        self.session.add(promotional)
+        self.session.commit()
+        self.session.refresh(promotional)
+        return promotional
+
+    def get_all_promotional(self) -> Optional[Promotional]:
+        return (
+            self.session.query(Promotional)
+            .all()
+        )
+
+    def get_promotional_by_id(self, promotional_id: int) -> Optional[Promotional]:
+        return self.session.query(Promotional).filter(Promotional.id == promotional_id).first()
+
+
+    def delete_promotional(self, promotional_id: int) -> bool:
+        promotional = self.session.query(Promotional).filter(Promotional.id == promotional_id).first()
+        if not promotional:
+            return False
+        self.session.delete(promotional)
+        self.session.commit()
+        return True
---
+++ app/routers/promotional.py
@@ -0,0 +1,39 @@
+from typing import List
+from fastapi import APIRouter, Depends, HTTPException, status, Header
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+
+from app.repository.promotional_repository import PromotionalRepository
+from app.utils.db import get_db
+from app.schemas.promotional_schemas import PromotionalCreate, PromotionalOut, PromotionalDelete
+
+router = APIRouter()
+
+@router.post("/create", response_model=PromotionalOut)
+def create_promotional(promotional_data: PromotionalCreate, db: Session = Depends(get_db)):
+    repo = PromotionalRepository(db)
+
+    new_promotional = repo.create_promotional(
+        promotion_name=promotional_data.promotion_name,
+        discount_type=promotional_data.discount_type,
+        discount_value=promotional_data.discount_value,
+        start_date=promotional_data.start_date,
+        end_date=promotional_data.end_date,
+        applicable_product_ids=promotional_data.applicable_product_ids
+    )
+    return new_promotional
+
+@router.get("/all", response_model=List[PromotionalOut])
+def get_all_promotional(db: Session = Depends(get_db)):
+    repo = PromotionalRepository(db)
+    promotional = repo.get_all_promotional()
+
+    return promotional
+
+@router.delete("/{promotional_id}", response_model=PromotionalDelete)
+def delete_promotional(promotional_id: int, db: Session = Depends(get_db)):
+    repo = PromotionalRepository(db)
+    deleted = repo.delete_promotional(promotional_id)
+    if not deleted:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Promotional not found")
+    return {"id": promotional_id}
