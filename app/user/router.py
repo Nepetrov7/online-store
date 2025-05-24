0a74e3ac361c534e0fa0e248f8d3f0859038f0ea
Vladislav Nozhkin <VNozhkin@exolve.ru>
2025-05-24 17:46:26 +0300

    added new endpoint for auth

--- app/user/router.py
+++ app/user/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from app.user.schema import TokenOut, UserLogin, UserRegister
+from app.user.schema import TokenOut, UserLogin, UserOut, UserRegister
 from app.user.service import UserService
 from app.utils.dependencies import get_current_user, get_db
 
@@ -22,6 +22,11 @@ def register_user(
     service: UserService = Depends(get_user_service),
 ):
     return service.register(user_in)
+
+
+@router.get("/current_user", response_model=UserOut, status_code=status.HTTP_200_OK)
+def get_current_user(current_user=Depends(get_current_user)):
+    return current_user
 
 
 @router.post(
